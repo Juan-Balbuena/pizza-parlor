e81d59a88af1e32d8b3c825d15d72057faed2d39
Noah Greensweig <ngreensweig@gmail.com>
2023-11-16 15:30:19 -0500

    built out index.js with reducers

--- src/index.js
+++ src/index.js
@@ -3,9 +3,43 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+import { Provider } from 'react-redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import logger from 'redux-logger';
+
+const cart = (state = [], action) => {
+
+    if (action.type === 'ADD_PIZZA') {
+        return [...state, action.payload];
+    }
+
+    return state;
+};
+
+const customerInfo = (state = [], action) => {
+
+    if (action.type === 'ADD_CUSTOMER_INFO') {
+        return [...state, action.payload];
+    }
+
+    return state;
+};
+
+const reduxStore = createStore(
+    combineReducers({
+        cart,
+        customerInfo,
+    }),
+    applyMiddleware(logger)
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={reduxStore}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
+
+export default reduxStore;
