dc6191afdb3d5462417d08cbebaca64236228f70
Ashley Wessel <amerino.dance@gmail.com>
2023-11-16 18:04:31 -0600

    continuing to work on customer form

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,17 +1,56 @@
-
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 function CustomerForm() {
-    //let [customerToAdd, setCustomerToAdd] = useState({name: 'test', });
+    const dispatch = useDispatch();
+
+    let [customerToAdd, setCustomerToAdd] = useState({
+        name: '', 
+        streetAddress: '',
+        city: '',
+        zip: '',
+        //type: ''
+     });
+
+
+     const handleNameChange = (event) => {
+        setCustomerToAdd({
+            ...customerToAdd,
+            name: event.target.value,
+        });
+     }
+
+     const handleAddressChange = (event) => {
+        setCustomerToAdd({
+            ...customerToAdd,
+            name: event.target.value,
+        });
+     }
+
+     const handleCityChange = (event) => {
+        setCustomerToAdd({
+            ...customerToAdd,
+            name: event.target.value,
+        });
+     }
+
+     const handleZipChange = (event) => {
+        setCustomerToAdd({
+            ...customerToAdd,
+            name: event.target.value,
+        });
+     }
+
+
+
 
 
     const addCustomerInfo = (event) => {
         console.log(`Adding customer info:`, customerToAdd)
         event.preventDefault();
-
-        // let action = {
-        //     type: 'ADD_CUSTOMER_INFO'
-        //     payload:
-        // }
+        
+        const action = { type: 'ADD_CUSTOMER_INFO', payload: setCustomerToAdd };
+         dispatch(action);
 
 
     };
@@ -23,26 +62,34 @@ function CustomerForm() {
         <h2>Customer Information</h2>
         <form onSubmit={(event) => addCustomerInfo(event)}>
             <input 
+                onChange={handleNameChange}
                 required
+                type='text'
                 placeholder="Name"
             />
             <br></br>
             <input 
+                onChange={handleAddressChange}
                 required
+                type='text'
                 placeholder="Street Address"
             />
             <br></br>
             <input 
+                onChange={handleCityChange}
                 required
+                type='text'
                 placeholder="City"
             />
             <br></br>
             <input 
+                onChange={handleZipChange}
                 required
+                type='text'
                 placeholder="Zip"
             />
             <br></br>
-            <input type="radio"/>
+            {/* <input type="radio"/> */}
             <br></br>
             <button type='submit'>
                 Next
