0c272a381f6ebaa10f38b2ae4763d71b14af1b88
Ashley Wessel <amerino.dance@gmail.com>
2023-12-04 01:18:54 -0600

    got the cards on pizza page to wrap

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,21 +1,56 @@
+// import React from 'react';
+// import { useSelector } from 'react-redux';
+// import PizzaItem from '../PizzaItem/PizzaItem';
+// import { Grid } from '@mui/material';
+
+// function PizzaList() {
+//     const pizzas = useSelector(store => store.pizzas)
+//     return (
+//         <Grid>
+            
+          
+//                 {
+//                     pizzas.map((pizza, i) => <PizzaItem key={i} pizza={pizza} />)
+//                 }
+        
+//         </Grid>
+
+//     )
+// }
+
+// export default PizzaList;
+
+
+// ^^ the above code is what we originally had, keeping it for now just in case
+
+
+
+
+
 import React from 'react';
 import { useSelector } from 'react-redux';
 import PizzaItem from '../PizzaItem/PizzaItem';
-import { Grid } from '@mui/material';
+import { Grid, Container } from '@mui/material';
 
 function PizzaList() {
     const pizzas = useSelector(store => store.pizzas)
     return (
-        <Grid>
-            
-          
+        <Container maxWidth="md">
+            <Grid container spacing={3}>
                 {
                     pizzas.map((pizza, i) => <PizzaItem key={i} pizza={pizza} />)
                 }
-        
-        </Grid>
+            </Grid>
+        </Container>
 
     )
 }
 
 export default PizzaList;
+
+
+
+
+
+
+
